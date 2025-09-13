9370e71d27436d08f97137ec7dd53918b93ace93
Mathieu <mathieu221605@yahoo.fr>
2025-09-13 20:39:01 +0200

    feat: operate function

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Var Declarations
+let firstNumber;
+let secondNumber;
+let operator;
+
+// Tests
+console.log("Testing operate, operate(1,2,\"+\") => " , operate(1,2,"+")); // Test
+
+
+
+function operate(x, y, operator){
+    switch (operator) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+        default:
+            return "Error! Operator type";
+    }
+}
+
 function add(x, y){
     return x + y;
 }
