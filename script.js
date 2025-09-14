97e17a642cd1999a9f07fc51b5c56d3842da5c92
SakolBien <mathieu221605@yahoo.fr>
2025-09-14 13:48:50 +0200

    feat: script working

--- script.js
+++ script.js
@@ -1,12 +1,92 @@
 // Var Declarations
-let firstNumber;
+const ERROR_STRING = "Error!";
+
+let firstNumber = 0;
 let secondNumber;
 let operator;
 
 // Tests
-console.log("Testing operate, operate(1,2,\"+\") => " , operate(1,2,"+")); // Test
 
+const buttons = document.getElementById("buttons");
+const screen = document.getElementById("screen")
 
+buttons.addEventListener("click", e => {
+    if(e.target.nodeName == "BUTTON"){
+        checkButtonType(e.target);
+        updateScreen();
+    }
+})
+
+// Functions
+
+function updateScreen(){
+    screen.textContent = firstNumber;
+    if(operator != undefined){
+        screen.textContent += operator;
+        if(secondNumber != undefined){
+            screen.textContent += secondNumber;
+        }
+    } 
+}
+
+function checkButtonType(eventTarget){
+    let buttonClicked = eventTarget.id;
+    if(firstNumber == ERROR_STRING) {
+        firstNumber = 0;
+    }
+
+    if(eventTarget.classList.contains("operator")) 
+    {
+        if(secondNumber == undefined)
+        {
+            operator = buttonClicked;
+        }else{
+            console.log("firstNumber: ", firstNumber, " secondNumber: ", secondNumber, " operator: ", operator);
+            const calcul = operate(firstNumber, secondNumber, operator);
+
+            operator = buttonClicked;
+
+            secondNumber = undefined;
+
+            firstNumber = calcul;
+        }
+    }
+    if(eventTarget.classList.contains("number")) 
+    {
+        buttonClicked = parseInt(buttonClicked);
+
+        if(operator == undefined) firstNumber = firstNumber * 10 + buttonClicked;
+        else {
+            if(secondNumber == undefined) secondNumber = buttonClicked;
+            else secondNumber = secondNumber * 10 + buttonClicked;
+        }
+    }
+
+    if(eventTarget.classList.contains("special")){
+        switch (buttonClicked) {
+            case "=":
+                if(secondNumber != undefined){
+                    firstNumber = operate(firstNumber, secondNumber, operator);
+                    secondNumber = undefined;
+                }
+                operator = undefined;
+                break;
+            case "clear":
+                firstNumber = 0;
+                secondNumber, operator = undefined;
+            case "back":
+                if(secondNumber != undefined){
+                    if(secondNumber < 10) secondNumber = undefined;
+                    else secondNumber = (secondNumber - secondNumber % 10) / 10;
+                }
+                else if(operator != undefined) operator = undefined;
+                else firstNumber = (firstNumber - firstNumber % 10) / 10;
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 function operate(x, y, operator){
     switch (operator) {
@@ -18,8 +98,13 @@ function operate(x, y, operator){
             return multiply(x, y);
         case "/":
             return divide(x, y);
+        case "%":
+            return modulo(x, y);
+        case undefined:
+            return x;
         default:
-            return "Error! Operator type";
+            console.error("Operator type is not defined in operate()!")
+            return ERROR_STRING;
     }
 }
 
@@ -36,6 +121,10 @@ function multiply(x, y){
 }
 
 function divide(x, y){
-    if(y == 0) return "ERROR!";
+    if(y == 0) return ERROR_STRING;
     return x / y;
+}
+
+function modulo(x, y){
+    return x % y;
 }
